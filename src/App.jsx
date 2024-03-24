3aa32376275fc3f186a25dc1e8601fd9c6887385
thstkddnr20 <thstkddnr20@naver.com>
2024-03-24 15:22:22 +0900

    props의 children에 대하여 배웠다

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 
 function App() {
@@ -24,6 +25,15 @@ function App() {
             image = {CORE_CONCEPTS[3].image}
           />
         </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
